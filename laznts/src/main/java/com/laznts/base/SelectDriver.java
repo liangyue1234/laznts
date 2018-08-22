7b3865a1918509d410fcbb624542987e228be6f5
liangyue1234 <17600045670@163.com>
2018-08-22 22:00:40 +0800

    la

---
+++ laznts/src/main/java/com/laznts/base/SelectDriver.java
@@ -0,0 +1,21 @@
+package com.laznts.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by Administrator on 2018/8/22.
+ */
+public class SelectDriver {
+       public WebDriver driverName(String browser){
+         if(browser.equalsIgnoreCase("chrome")){
+             System.setProperty("webdriver.chrome.driver","C:\\Users\\Administrator\\Desktop\\driver\\chromedriver.exe");
+             return new ChromeDriver();
+         }else {
+             System.setProperty("webdriver.firefox.driver","C:\\Users\\Administrator\\Desktop\\driver\\geckodriver.exe");
+             return new FirefoxDriver();
+         }
+       }
+
+}
